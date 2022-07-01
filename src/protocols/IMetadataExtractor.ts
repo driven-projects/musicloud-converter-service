3c8d7aa0855c1e8d8de0f7e3ddec8e3aeba881bb
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-01 13:25:46 -0300

    feat: definition of metadata extractor interface

---
+++ src/protocols/IMetadataExtractor.ts
@@ -0,0 +1,11 @@
+interface IMetadata {
+  title?: string;
+  artist?: string;
+  album?: string;
+  genres?: string[];
+  year?: number;
+}
+
+export default interface IMetadataExtractor {
+  parse(file: string): Promise<IMetadata>;
+}

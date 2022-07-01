de9d5a7876479c6f7bb6f2421fa9e250df40286d
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-01 13:27:18 -0300

    chore: testing implemented adapters

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello!");
+import MetadataExtractor from "@/adapters/MetadataExtractor";
+import FileConverter from "./adapters/FileConverter";
+
+const converter = new FileConverter();
+const extractor = new MetadataExtractor();
+
+(async () => {
+  await converter.convertToMp3("./Mechanix.wav", "./Mechanix 3.mp3");
+  const metadata = await extractor.parse("./Mechanix 3.mp3");
+  console.log(metadata);
+})();

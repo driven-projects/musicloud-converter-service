20336335a99c9fe9e7e4ec3d25c3440492506c10
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-05 11:57:44 -0300

    feat: working system using use case

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
+import "./setup";
+
 import MetadataExtractor from "@/adapters/MetadataExtractor";
 import FileConverter from "@/adapters/FileConverter";
+import BucketProvider from "@/adapters/BucketProvider";
 
 import SongConverter from "@/domain/SongConverter";
+import Bucket from "@/domain/Bucket";
 
-const converter = new FileConverter();
-const extractor = new MetadataExtractor();
-const songConverter = new SongConverter(extractor, converter);
+import ProcessSongUseCase from "@/usecases/ProcessSongUseCase";
 
-(async () => {
-  const info = await songConverter.convert("./08. Mechanix.mp3");
-  console.log(info);
-})();
+class ConverterService {
+  #processSongUseCase: ProcessSongUseCase
+
+  constructor() {
+    this.#processSongUseCase = this.#buildProcessSongUseCase();
+  }
+
+  async run() {
+    await this.#processSongUseCase.execute("Mechanix.wav");
+  }
+
+  #buildProcessSongUseCase() {
+    const converter = new FileConverter();
+    const extractor = new MetadataExtractor();
+    const songConverter = new SongConverter(extractor, converter);
+
+    const bucketProvider = new BucketProvider(
+      process.env.AWS_REGION,
+      process.env.AWS_ACCESS_KEY_ID,
+      process.env.AWS_SECRET_ACCESS_KEY,
+      process.env.AWS_BUCKET_NAME
+    );
+    const bucket = new Bucket(bucketProvider);
+
+    const useCase = new ProcessSongUseCase(songConverter, bucket);
+    return useCase;
+  }
+}
+
+new ConverterService().run();
